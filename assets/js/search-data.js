17f2a3513bd5ed5c9d0bbfaf1db3b6b0a5cbe013
fansunqi <73486033+fansunqi@users.noreply.github.com>
2025-04-02 11:26:12 +0000

    Deploying to gh-pages from @ fansunqi/fansunqi.github.io@eb73931acea3d87a529820d9599b4aa73a136a9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,13 +440,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-flexkbqa-is-accepted-by-aaai-2024-as-oral-presentation",
           title: 'FlexKBQA is accepted by AAAI 2024 as oral presentation.',
+          description: "",
+          section: "News",},{id: "news-begin-my-journey-to-pursue-a-cs-ph-d",
+          title: 'Begin my journey to pursue a CS Ph.D.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -498,14 +496,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%73%74%65%70%68%65%6E%73%75%6E%71%69%66%61%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -520,13 +511,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
