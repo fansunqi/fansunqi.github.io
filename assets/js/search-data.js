5d312e99ffc8290d786194325e1de2bcded4cc92
fansunqi <73486033+fansunqi@users.noreply.github.com>
2025-04-02 11:22:50 +0000

    Deploying to gh-pages from @ fansunqi/fansunqi.github.io@ae4ed610739c77e2cb959750e92b2b8c69f1add5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,13 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-flexkbqa-is-accepted-by-aaai-2024-as-oral-presentation",
+          title: 'FlexKBQA is accepted by AAAI 2024 as oral presentation.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
