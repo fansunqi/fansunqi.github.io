536c1a624b15c4aa87ae48a2a0e048c9a64ea452
fansunqi <73486033+fansunqi@users.noreply.github.com>
2025-04-10 07:30:15 +0000

    Deploying to gh-pages from @ fansunqi/fansunqi.github.io@2333829459bfeb95fbfa60d415134204dbe8c88f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "publications by time.",
+          description: "publications and preprints by year.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
